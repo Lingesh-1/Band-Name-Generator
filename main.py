a4477cd61153362a8392507bd23be1944f70a4d2
Lingesh.R <146902022+Lingesh-1@users.noreply.github.com>
2024-05-15 21:30:33 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+#Band generator
+print("Welcomee to Band Name generator")
+name=input("What's the name of the city you grew up in:\n")
+color=input("What's your pet's name:\n")
+
+print("Your band name could be",'"',name,color,'"')
